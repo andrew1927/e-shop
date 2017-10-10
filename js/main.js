9609f1d3dab429f0be5c89858118d12ce2b54f10
Molodetskyi Andrii <amolodetskyi@gmail.com>
2017-10-10 19:53:02 +0200

    2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-var elTbody = document.getElementsByTagName("tbody")[0];
+var elTbody = document.getElementsByTagName("tbody")[0],
+	elPageAddNewProduct = document.getElementsByClassName("popup")[0],
+	elBtnAddProduct = document.getElementsByTagName("button")[0],
+	elBtnSave = document.getElementsByClassName("save")[0];
+	elBtnCancel = document.getElementsByClassName("cancel")[0];
 
 init(products);
 function init(arr){
@@ -15,7 +19,45 @@ function addRow(obj){
 function createInfoInner(obj){
 	var elTr = document.createElement("tr");
 
-	elTr.innerHTML = `<td>${obj.name}</td><td>${obj.description}</td><td>${obj.price}</td>`;
+	elTr.innerHTML = "<td>"+obj.name+"</td><td>"+obj.description+"</td><td>"+obj.price+"</td>";
 	return elTr;
 }
 
+
+
+function addProduct(){
+
+	var elName = document.getElementsByClassName("name")[0],
+		elDescription = document.getElementsByClassName("description")[0],
+		elSdescription = document.getElementsByClassName("sdescription")[0],
+		elPrice = document.getElementsByClassName("price")[0];
+	
+	var newProduct = {};
+
+	newProduct.name = elName.value;
+	newProduct.description = elDescription.value;
+	newProduct.shortDescription = elSdescription.value;
+	newProduct.price = elPrice.value;
+
+	products.push(newProduct);	
+	console.log(products);
+
+	elBtnAddProduct.addEventListener("click", showPopup);
+
+	function showPopup(){
+		elPageAddNewProduct.classList.add("show");
+	}
+	function closePopup(){
+		elPageAddNewProduct.classList.remove("show");
+		elName.value = "";
+		elDescription.value = "";
+		elSdescription.value = "";
+		elPrice.value = "";
+	}
+
+	elBtnSave.addEventListener("click", addProduct);
+	elBtnSave.addEventListener("click", addRow(newProduct));
+	elBtnSave.addEventListener("click", closePopup);
+	elBtnCancel.addEventListener("click",closePopup);
+}
+addProduct();
