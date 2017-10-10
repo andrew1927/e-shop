ffe7212330c74042f0a0c79d70f69634d1613c51
Molodetskyi Andrii <amolodetskyi@gmail.com>
2017-10-10 21:14:58 +0200

    3

--- js/data.js
+++ js/data.js
@@ -2,16 +2,19 @@ var products = [
   {
     name: "Nexus 5",
     description: "Android 5.0, 8GB, GPS...",
+    shortDescription: "Android 5.0, 8GB, GPS...",
     price: 4000.00
   },
   {
     name: "Redmi 4x",
     description: "Android 6.0, 32GB, GPS...",
+    shortDescription: "Android 6.0, 32GB, GPS...",
     price: 10000.00
   },
   {
     name: "iPhone 5",
     description: "iOS 8, 32GB, 5",
+    shortDescription: "iOS 8, 32GB, 5",
     price: 12000.00
   }
 ]
--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ var elTbody = document.getElementsByTagName("tbody")[0],
 	elBtnSave = document.getElementsByClassName("save")[0];
 	elBtnCancel = document.getElementsByClassName("cancel")[0];
 
+var elName = document.getElementsByClassName("name")[0],
+	elDescription = document.getElementsByClassName("description")[0],
+	elSdescription = document.getElementsByClassName("sdescription")[0],
+	elPrice = document.getElementsByClassName("price")[0];
+
 init(products);
+
 function init(arr){
 	arr.forEach(function(item){
 		addRow(item);
@@ -19,45 +25,38 @@ function addRow(obj){
 function createInfoInner(obj){
 	var elTr = document.createElement("tr");
 
-	elTr.innerHTML = "<td>"+obj.name+"</td><td>"+obj.description+"</td><td>"+obj.price+"</td>";
+	elTr.innerHTML = "<td>"+obj.name+"</td><td>"+obj.shortDescription+"</td><td>"+obj.price+"</td>";
 	return elTr;
 }
 
+elBtnSave.addEventListener("click", addProduct);
+elBtnAddProduct.addEventListener("click", showPopup);
+elBtnCancel.addEventListener("click",closePopup);
 
-
-function addProduct(){
-
-	var elName = document.getElementsByClassName("name")[0],
-		elDescription = document.getElementsByClassName("description")[0],
-		elSdescription = document.getElementsByClassName("sdescription")[0],
-		elPrice = document.getElementsByClassName("price")[0];
-	
-	var newProduct = {};
-
-	newProduct.name = elName.value;
-	newProduct.description = elDescription.value;
-	newProduct.shortDescription = elSdescription.value;
-	newProduct.price = elPrice.value;
-
-	products.push(newProduct);	
-	console.log(products);
-
-	elBtnAddProduct.addEventListener("click", showPopup);
-
-	function showPopup(){
-		elPageAddNewProduct.classList.add("show");
-	}
-	function closePopup(){
-		elPageAddNewProduct.classList.remove("show");
-		elName.value = "";
-		elDescription.value = "";
-		elSdescription.value = "";
-		elPrice.value = "";
-	}
-
-	elBtnSave.addEventListener("click", addProduct);
-	elBtnSave.addEventListener("click", addRow(newProduct));
-	elBtnSave.addEventListener("click", closePopup);
-	elBtnCancel.addEventListener("click",closePopup);
+function Product(name,description,shortDescription,price){
+	this.name = name;
+	this.description = description;
+	this.shortDescription = shortDescription;
+	this.price = price;
 }
-addProduct();
+
+function addProduct(event){
+	event.preventDefault();
+	var newProduct = new Product(elName.value,elDescription.value,elSdescription.value,elPrice.value);
+	products.push(newProduct);	
+	
+	addRow(newProduct);
+	closePopup();
+}
+
+function showPopup(){
+	elPageAddNewProduct.classList.add("show");
+}
+
+function closePopup(){
+	elPageAddNewProduct.classList.remove("show");
+	elName.value = "";
+	elDescription.value = "";
+	elSdescription.value = "";
+	elPrice.value = "";
+}
