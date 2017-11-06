1211c2d5aa3eb20a10eae8b07046a50919a92952
Molodetskyi Andrii <amolodetskyi@gmail.com>
2017-11-06 19:53:43 +0100

    :add

--- js/data.js
+++ js/data.js
@@ -1,17 +1,20 @@
-var products = [
+var data = [
   {
+    src: "images/nexus.jpg",
     name: "Nexus 5",
     description: "Android 5.0, 8GB, GPS...",
     shortDescription: "Android 5.0, 8GB, GPS...",
     price: 4000.00
   },
   {
+    src: "images/mi.jpg",
     name: "Redmi 4x",
     description: "Android 6.0, 32GB, GPS...",
     shortDescription: "Android 6.0, 32GB, GPS...",
     price: 10000.00
   },
   {
+    src: "images/iphone.jpg",
     name: "iPhone 5",
     description: "iOS 8, 32GB, 5",
     shortDescription: "iOS 8, 32GB, 5",
--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 var elTbody = document.getElementsByTagName("tbody")[0],
 	elPageAddNewProduct = document.getElementsByClassName("popup")[0],
 	elBtnAddProduct = document.getElementsByTagName("button")[0],
-	elBtnSave = document.getElementsByClassName("save")[0];
-	elBtnCancel = document.getElementsByClassName("cancel")[0];
+	elBtnSaveProduct = document.getElementsByClassName("save-product")[0];
+	elBtnCancel = document.getElementsByClassName("cancel")[0],
+	elBtnSaveEditProduct = document.getElementsByClassName("save-edit-product")[0],
+	elAddImg = document.getElementById("getimage"); 
+
 
 var elName = document.getElementsByClassName("name")[0],
 	elDescription = document.getElementsByClassName("description")[0],
 	elSdescription = document.getElementsByClassName("sdescription")[0],
 	elPrice = document.getElementsByClassName("price")[0];
+
+var currentIndex;	
+
+var products = JSON.parse(localStorage.getItem("key"));
+
+if(!products){
+	products = data;
+	var serialProducts = JSON.stringify(products);
+	localStorage.setItem("key", serialProducts);
+};
 
 init(products);
 
@@ -25,48 +38,85 @@ function addRow(obj,index){
 function createInfoInner(obj,index){
 	var elTr = document.createElement("tr");
 
-	elTr.innerHTML = "<td>"+obj.name+"</td><td>"+obj.shortDescription+"</td><td>"+obj.price+"</td><td><button class='edit'>EDIT</button></td><td><button class='delete'>DELETE</button></td>";
+	elTr.innerHTML = "<td><img src="+obj.src+" class='product-img'></td><td>"+obj.name+"</td><td>"+obj.shortDescription+"</td><td>"+obj.description+"</td><td>"+obj.price+"</td><td><button class='edit'>EDIT</button></td><td><button class='delete'>DELETE</button></td>";
 
 	var elBtnDelete = elTr.querySelector(".delete"),
 		elBtnEdit = elTr.querySelector(".edit");
 
 	elBtnDelete.addEventListener("click", function(){
 		elTbody.removeChild(elTr);
+		
 		products.splice(index,1);
+
+		rewriteLocalStorage();
 	});
 
 	elBtnEdit.addEventListener("click", function(){
-		showPopup();
+
+		currentIndex = index;
+
 		elName.value = obj.name;
 		elDescription.value = obj.description;
 		elSdescription.value = obj.shortDescription;
 		elPrice.value = obj.price;
 
-	elBtnSave.addEventListener("click", function(){
-		alert("hi");
+		showPopup();
+
+		elBtnSaveProduct.classList.add("hide");
+		elBtnSaveEditProduct.classList.remove("hide");
 	});
 
-	})
 	return elTr;
 } 
 
-elBtnSave.addEventListener("click", addProduct);
-elBtnAddProduct.addEventListener("click", showPopup);
-elBtnCancel.addEventListener("click",closePopup);
+elBtnSaveProduct.addEventListener("click", saveProduct);
+elBtnAddProduct.addEventListener("click", function(){
+	showPopup();
+	elBtnSaveProduct.classList.remove("hide");
+	elBtnSaveEditProduct.classList.add("hide");	
+});
+elBtnCancel.addEventListener("click", closePopup);
+elBtnSaveEditProduct.addEventListener("click", saveEditProduct);
 
-function Product(name,description,shortDescription,price){
+elAddImg.addEventListener("change", loadImage);
+
+function Product(src, name,description,shortDescription,price){
+	this.src = src;
 	this.name = name;
 	this.description = description;
 	this.shortDescription = shortDescription;
 	this.price = price;
 }
 
-function addProduct(event){
+function saveProduct(event){
 	event.preventDefault();
 	var newProduct = new Product(elName.value,elDescription.value,elSdescription.value,elPrice.value);
-	products.push(newProduct);	
-	
+	products.push(newProduct);
+
+	rewriteLocalStorage();
+
 	addRow(newProduct);
+	closePopup();
+}
+
+function saveEditProduct(event){
+	event.preventDefault();
+	products[currentIndex] = {
+		name: elName.value,
+		description: elDescription.value,
+		shortDescription: elSdescription.value,
+		price: elPrice.value
+	}
+
+	products.splice(currentIndex, products.currentIndex);
+
+	while(elTbody.firstChild){
+		elTbody.removeChild(elTbody.firstChild);
+	}
+
+	rewriteLocalStorage();
+	init(products);
+
 	closePopup();
 }
 
@@ -80,5 +130,23 @@ function closePopup(){
 	elDescription.value = "";
 	elSdescription.value = "";
 	elPrice.value = "";
+} 
+
+function rewriteLocalStorage(){
+	var serialProducts = JSON.stringify(products);
+	localStorage.setItem("key", serialProducts);
 }
+
+function loadImage(event){
+	var fileName = event.target.files[0];
+	var fr = new FileReader();
+	fr.onload = event.target.result;
+	fr.readAsDataURL(filename);  
+}
+
+// event - Интерфейс Event представляет собой любое событие, которое происходит в DOM, Event содержит общие свойства и методы для всех событий.
+// event.target - Ссылка на целевой объект, на котором произошло событие.
+// change - Событие change происходит по окончании изменения значения элемента формы, когда это изменение зафиксировано.
+
+
 
